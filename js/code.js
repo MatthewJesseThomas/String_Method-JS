d05896d8a51922622574820db03e5bc29ec4804c
MatthewJesseThomas <owlsaintgermain@gmail.com>
2022-11-18 14:51:51 +0200

    main

---
+++ js/code.js
@@ -0,0 +1,22 @@
+let paragraph = document.querySelector('#paragraphic').textContent;
+
+let index = 0;
+
+// console.log(paragraph[0]);
+// console.log(`${paragraph} ${paragraph.length}`);
+// console.log(paragraph.match('Greek'));
+// console.log(paragraph.repeat('to'));
+// console.log(paragraph.replace('F','P'));
+// console.log(paragraph.search('BC')); 
+// console.log(paragraph.slice('Hellenistic')); 
+// console.log(paragraph.split('Greek')); 
+// console.log(paragraph.startsWith('period')); 
+// console.log(paragraph.substr('124')); 
+// console.log(paragraph.substring('0000')); 
+// console.log(paragraph.toLowerCase('BC')); 
+// console.log(paragraph.toUpperCase('BC')); 
+// console.log(paragraph.toString('6th')); 
+// console.log(paragraph.trim('Hellenistic')); 
+// console.log(paragraph.trimEnd('Hellenistic')); 
+// console.log(paragraph.trimStart('Hellenistic')); 
+// console.log(paragraph.valueOf('Empire'));  
